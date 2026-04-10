49cc6bc839e2936c5aab95d67d5c2ab4de2f3547
Benas Vaiciulis <benasvaiciulis@gmail.com>
2026-04-10 21:07:55 +1000

    chore: ROS2 package boilerplate — package.xml, setup.py, setup.cfg, resource marker

---
+++ src/langrobot/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'langrobot'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'worlds'), glob('../../worlds/*.sdf')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Benas Vaiciulis',
+    maintainer_email='benas@example.com',
+    description='Language-driven robot manipulation system',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'controller_node = langrobot.controller_node:main',
+        ],
+    },
+)
